c1082386facc5a6a01a0b60fa214107fb81ad3dd
adithya-902 <74303205+adithya-902@users.noreply.github.com>
2023-05-27 20:33:56 +0530

    started on navbar

--- bloglog/src/App.js
+++ bloglog/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <div className="navbar">
+        <ul className="menu-items">
+          <li className="logo">logo</li>
+          <li className="Home">Home</li>
+          <li className="About">About</li>
+          <li className="Blog">Services</li>
+          <li className="Contact">Contact</li>
+        </ul>
+      </div>
     </div>
   );
 };
